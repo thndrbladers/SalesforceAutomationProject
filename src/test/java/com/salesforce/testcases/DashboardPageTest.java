41b107158bf6c22c5a8cfff0e045d5e0ec2ce26b
dummythndrbladers <thndrbladers@gmail.com>
2022-05-16 23:15:52 +0530

    added listeners, added util, added testng regression sanity xml

--- src/test/java/com/salesforce/testcases/DashboardPageTest.java
+++ src/test/java/com/salesforce/testcases/DashboardPageTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -14,6 +15,7 @@ import com.salesforce.pages.DashboardPage;
 import com.salesforce.pages.LoginPage;
 import com.salesforce.util.NALExcelXLSReader;
 
+@Listeners(com.salesforce.util.ExtentReporterNG.class)
 public class DashboardPageTest extends TestBase {
 
 	DashboardPage dashboardPage;
@@ -31,7 +33,7 @@ public class DashboardPageTest extends TestBase {
 
 	}
 
-	@Test(groups = { "sanity", "regression", "negative" })
+	@Test(groups = { "sanity", "regression"})
 	public void verifyAllTabsOnDashboardPage() {
 
 		/*
